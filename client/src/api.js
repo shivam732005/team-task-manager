144872cfefdf52cdad52fd88eb893207492e94aa
shivam pal <sp1077911@gmail.com>
2026-05-04 16:01:09 +0530

    Fixed production API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://team-task-manager-production-2786.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
